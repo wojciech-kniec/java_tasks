b4450277cdd72f89dc450927940aab6999210d73
wojciech-kniec <kniecwojciech@gmail.com>
2019-06-12 17:16:28 +0200

    mapChanger method works with a String paramter.

--- src/main/java/tasks/Map.java
+++ src/main/java/tasks/Map.java
@@ -1,11 +1,14 @@
 package tasks;
 
 public class Map {
-    public static void main(String args[]) throws Exception {
-        String s = "qqqq";
+    public static void main(String args[]) {
+    }
+
+    byte[] mapChanger(String s) throws Exception {
         byte arr[] = s.getBytes("UTF8");
-        for (byte x: arr) {
-            System.out.print(x+" ");
+        for (byte x : arr) {
+            System.out.print(x + " ");
         }
+        return arr;
     }
 }
